e631a8d1f85cecdd2ffd668510e65d4d16b53aa4
ValentinoHere <foxkitsune33@gmail.com>
2024-10-15 16:09:59 +0700

    add testimonials and fix navbar

---
+++ js/script.js
@@ -0,0 +1,46 @@
+document.addEventListener("alpine:init", () => {
+  Alpine.data("testimonials", () => ({
+    items: [
+      {
+        img: "assets/avatar_1.png",
+        desc: " “Wow... I am very happy to use this VPN, it turned out to be more than my expectations and so far there have been no problems. LaslesVPN always the best”.",
+      },
+      {
+        img: "assets/avatar_1.png",
+        desc: " “Wow... I am very happy to use this VPN, it turned out to be more than my expectations and so far there have been no problems. LaslesVPN always the best”.",
+      },
+      {
+        img: "assets/avatar_1.png",
+        desc: " “Wow... I am very happy to use this VPN, it turned out to be more than my expectations and so far there have been no problems. LaslesVPN always the best”.",
+      },
+      {
+        img: "assets/avatar_1.png",
+        desc: " “Wow... I am very happy to use this VPN, it turned out to be more than my expectations and so far there have been no problems. LaslesVPN always the best”.",
+      },
+      {
+        img: "assets/avatar_1.png",
+        desc: " “Wow... I am very happy to use this VPN, it turned out to be more than my expectations and so far there have been no problems. LaslesVPN always the best”.",
+      },
+      {
+        img: "assets/avatar_1.png",
+        desc: " “Wow... I am very happy to use this VPN, it turned out to be more than my expectations and so far there have been no problems. LaslesVPN always the best”.",
+      },
+      {
+        img: "assets/avatar_1.png",
+        desc: " “Wow... I am very happy to use this VPN, it turned out to be more than my expectations and so far there have been no problems. LaslesVPN always the best”.",
+      },
+      {
+        img: "assets/avatar_1.png",
+        desc: " “Wow... I am very happy to use this VPN, it turned out to be more than my expectations and so far there have been no problems. LaslesVPN always the best”.",
+      },
+      {
+        img: "assets/avatar_1.png",
+        desc: " “Wow... I am very happy to use this VPN, it turned out to be more than my expectations and so far there have been no problems. LaslesVPN always the best”.",
+      },
+      {
+        img: "assets/avatar_1.png",
+        desc: " “Wow... I am very happy to use this VPN, it turned out to be more than my expectations and so far there have been no problems. LaslesVPN always the best”.",
+      },
+    ],
+  }));
+});
